49e1b98cc8fc72c5f466cdbb4fedef8238486afa
Michael Smith <owlstronaut@github.com>
2026-03-25 14:59:17 -0700

    chore: update eslint, make compatible

--- eslint.config.js
+++ eslint.config.js
@@ -1,8 +1,12 @@
-// eslint.config.js - Simple ESLint v9 flat config
+// eslint.config.js - ESLint v10 flat config
 import tsParser from '@typescript-eslint/parser';
 import tsPlugin from '@typescript-eslint/eslint-plugin';
+import eslintConfigPrettier from 'eslint-config-prettier/flat';
 
 export default [
+  {
+    ignores: ['dist/**', 'node_modules/**', 'coverage/**'],
+  },
   {
     files: ['**/*.ts'],
     languageOptions: {
@@ -25,24 +29,9 @@ export default [
   {
     // Allow explicit any types in test files
     files: ['**/*.test.ts', '**/test/**/*.ts', '**/__tests__/**/*.ts'],
-    languageOptions: {
-      parser: tsParser,
-      ecmaVersion: 2020,
-      sourceType: 'module',
-    },
-    plugins: {
-      '@typescript-eslint': tsPlugin,
-    },
     rules: {
-      '@typescript-eslint/no-unused-vars': [
-        'error',
-        { argsIgnorePattern: '^_' },
-      ],
-      '@typescript-eslint/explicit-function-return-type': 'off',
-      '@typescript-eslint/no-explicit-any': 'off', // Allow any types in test files
+      '@typescript-eslint/no-explicit-any': 'off',
     },
   },
-  {
-    ignores: ['dist/**', 'node_modules/**', 'coverage/**'],
-  },
+  eslintConfigPrettier,
 ];
